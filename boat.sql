f54d026950cceb1ef41c7410790319c049906eaf
chetan-gadekar <131698084+chetan-gadekar@users.noreply.github.com>
2023-04-30 12:06:06 +0530

    Update boat.sql

--- boat.sql
+++ boat.sql
@@ -1,4 +1,52 @@
-/* here we update the rating */  
+/* here we update the rating */ 
+delimiter //
+create trigger rateing
+before insert on reserve
+for each row
+begin
+call up_rating(new.sid);
+end //
+delimiter ;
+
+
+/*
+mysql> select * from reserve;
++------+------+------------+
+| bid  | sid  | dor        |
++------+------+------------+
+|  101 |    1 | 2023-10-12 |
+|  102 |    2 | 2024-10-11 |
+|  101 |    1 | 2023-12-10 |
+|  101 |    1 | 2023-03-04 |
+|  101 |    1 | 2023-04-04 |
+|  101 |    1 | 2023-03-04 |
++------+------+------------+
+6 rows in set (0.00 sec)
+mysql> select * from boats;
++------+-------+--------+
+| bid  | bname | bcolor |
++------+-------+--------+
+|  101 | b1    | red    |
+|  102 | b2    | black  |
++------+-------+--------+
+2 rows in set (0.02 sec)
+mysql> insert into reserve values(101,1,'2024-10-12');
+Query OK, 1 row affected (0.08 sec)
+mysql> select * from reserve;
++------+------+------------+
+| bid  | sid  | dor        |
++------+------+------------+
+|  101 |    1 | 2023-10-12 |
+|  102 |    2 | 2024-10-11 |
+|  101 |    1 | 2023-12-10 |
+|  101 |    1 | 2023-03-04 |
+|  101 |    1 | 2023-04-04 |
+|  101 |    1 | 2023-03-04 |
+|  101 |    1 | 2024-10-12 |
++------+------+------------+
+7 rows in set (0.00 sec)
+*/
+
 Delimiter //
 create procedure up_rating1(in s int)
 begin
@@ -7,8 +55,6 @@ end //
 delimiter ; 
 
 /*
-mysql> select * from silors;
-ERROR 1146 (42S02): Table 'HRS.silors' doesn't exist
 mysql> select * from sailors;
 +------+--------+---------+
 | sid  | sname  | srating |
@@ -18,10 +64,7 @@ mysql> select * from sailors;
 +------+--------+---------+
 2 rows in set (0.00 sec)
 
-mysql> source /home/student/boat.sql
-ERROR 1304 (42000): PROCEDURE up_rating already exists
-mysql> source /home/student/boat.sql
-Query OK, 0 rows affected (0.14 sec)
+
 
 mysql> call up_rating1(1);
 Query OK, 1 row affected (0.19 sec)
